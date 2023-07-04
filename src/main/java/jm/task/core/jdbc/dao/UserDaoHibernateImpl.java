dc0b98c50e31c094838ce0d90ff6b7554a5589be
GitHubDANIIL <129953722+GitHubDANIIL@users.noreply.github.com>
2023-07-04 15:46:39 +0300

    add Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
@@ -9,34 +12,79 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
+    private static SessionFactory sessionFactory = Util.getSession();
 
     @Override
     public void createUsersTable() {
+        String createTable = "CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL," +
+                "name VARCHAR(45) NOT NULL," +
+                "lastname VARCHAR(45) NOT NULL," +
+                "age TINYINT(10) NOT NULL)";
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(createTable).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
+        String dropTable = "DROP TABLE IF EXISTS users";
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(dropTable).addEntity(User.class).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
 
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.remove(session.get(User.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String getAll = "FROM User";
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            List<User> users = session.createQuery(getAll).getResultList();
+            session.getTransaction().commit();
+            return users;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        String clearTable = "delete from User";
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createQuery(clearTable).executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,16 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/pp_1.1.4";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1.1.5-hibernate";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static Connection connection;
@@ -20,5 +23,10 @@ public class Util {
             throw new RuntimeException(e);
         }
         return connection;
+    }
+
+    public static SessionFactory getSession() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory();
     }
 }
